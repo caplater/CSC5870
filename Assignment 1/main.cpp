ec642977678acb84feef2a7b8e7db6be3459d5a6
ab3189 <TcqbqB4k%4wnwd>
2018-10-13 18:55:18 -0400

    Initial Commit

---
+++ Assignment 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 1
+//
+//  Created by Charles Plater on 10/13/18.
+//  Copyright © 2018 Charles Plater. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
